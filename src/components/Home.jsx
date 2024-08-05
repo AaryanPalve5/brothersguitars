ff154e4663b4ecbe84565d22cbe79cac14b31af5
AaryanPalve5 <aspalve2004@gmail.com>
2024-08-06 01:10:56 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is import
 
 function Home() {
     return (
-        <div className="container-fluid">
-            <div className="row justify-content-center align-items-center" style={{ height: '100vh' }}>
-                <div className="col-md-8 d-flex justify-content-center">
-                    <img
+        <>
+       <img
                         src="src\components\brothersguitarslogo.jpeg"
                         alt="Brothers Guitars Logo"
                         className="img-fluid"
                         style={{ maxWidth: '100%', height: 'auto' }}
                     />
-                </div>
-            </div>
             <h1>Brothers Guitars </h1>
-            
-        </div>
+            </>
     );
 }
 
